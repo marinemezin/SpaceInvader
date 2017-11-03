9b9de37bc0cbed821603d964b18b217af5c9094a
marinemarine <mezin.marine@gmail.com>
2017-11-03 15:28:46 -0400

    CVaisseau

---
+++ Laboratoire3/CVaisseau.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include <thread>
+#include <mutex>
+
+using namespace std;
+
+#include "CVaisseau.h"
+#include "CEcran.h"
+
+mutex CVaisseau::Verrou;
+
+CVaisseau::CVaisseau()
+{
+
+}
+
+CVaisseau::~CVaisseau()
+{
+
+}
+
+void CVaisseau::deplacer()
+{
+
+}
