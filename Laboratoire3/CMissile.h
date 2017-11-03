1928f0e6410542845444c6f228300c970bf32e0a
marinemarine <mezin.marine@gmail.com>
2017-11-03 14:32:31 -0400

    CMIssile.h

--- Laboratoire3/CMissile.h
+++ Laboratoire3/CMissile.h
@@ -1,5 +1,19 @@
 #pragma once
+
+#include <thread>
+#include <mutex>
+
 class CMissile 
 {
+private:
+	int Colonne;
+	int Ligne;
+	std::thread* LeThread;
+
+	void monter();
 	
+	static int Verrou;
+public:
+	CMissile();
+	~CMissile();	
 };
